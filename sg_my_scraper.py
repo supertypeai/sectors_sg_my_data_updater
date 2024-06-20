61650a563573b686e366c8c390df8fc1fd068268
egen <christeigentheodore@gmail.com>
2024-06-20 22:36:29 +0700

    update sg_my_scraper.py

--- sg_my_scraper.py
+++ sg_my_scraper.py
@@ -18,15 +18,15 @@ def GetGeneralData(country):
     elif country == "my":
         url = "https://api.investing.com/api/financialdata/assets/equitiesByCountry/default?fields-list=id%2Cname%2Csymbol%2CisCFD%2Chigh%2Clow%2Clast%2ClastPairDecimal%2Cchange%2CchangePercent%2Cvolume%2Ctime%2CisOpen%2Curl%2Cflag%2CcountryNameTranslated%2CexchangeId%2CperformanceDay%2CperformanceWeek%2CperformanceMonth%2CperformanceYtd%2CperformanceYear%2Cperformance3Year%2CtechnicalHour%2CtechnicalDay%2CtechnicalWeek%2CtechnicalMonth%2CavgVolume%2CfundamentalMarketCap%2CfundamentalRevenue%2CfundamentalRatio%2CfundamentalBeta%2CpairType&country-id=42&filter-domain=&page=0&page-size=2000&limit=0&include-additional-indices=false&include-major-indices=false&include-other-indices=false&include-primary-sectors=false&include-market-overview=false"
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+    data_from_api = None
     for i in range(10):
+        print(f"iterasi ke-{i+1}")
         response = requests.get(url, headers=headers)
         if response.status_code == 200:
             json_data = response.json()
-            data = pd.DataFrame(json_data["data"])
+            data_from_api = pd.DataFrame(json_data["data"])
             break
-        else:
-            continue
-    return data
+    return data_from_api
 
 def GetAdditionalData(links):
     data_list = []
