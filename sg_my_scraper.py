9a308f9b409dab3faf086f821979788d9f289f4c
egen <christeigentheodore@gmail.com>
2024-06-20 18:39:14 +0700

    add github action and update sg_my_scraper.py

--- sg_my_scraper.py
+++ sg_my_scraper.py
@@ -134,108 +134,234 @@ def convert_to_number(x):
     else:
         return np.nan
 
-def rename_and_convert(data):
-    rename_cols = {
-        'Name' : 'name', 
-        'Symbol' : 'symbol',
-        'currency' : 'currency',
-        'Sector' : 'sector', 
-        'Industry' : 'industry', 
-        'close' : 'close', 
-        'change_percent' : 'percentage_change',
-        'Market Cap' : 'market_cap', 
-        'Volume_y' : 'volume', 
-        'P/E Ratio' : 'pe', 
-        'Revenue' : 'revenue',
-        'EPS' : 'eps',
-        'FundamentalBeta' : 'beta', 
-        'dividend' : 'dividend',
-        'dividend_yield' : 'dividend_yield',
-        'TechnicalDay' : 'daily_signal',
-        'TechnicalWeek' : 'weekly_signal', 
-        'TechnicalMonth' : 'monthly_signal',
-        'PerformanceDay' : 'daily_percentage_change', 
-        'PerformanceWeek' : 'weekly_percentage_change',
-        'PerformanceMonth' : 'monthly_percentage_change', 
-        'PerformanceYtd' : 'ytd_percentage_change',
-        'PerformanceYear' : 'one_year_percentage_change', 
-        'Performance3Year' : 'three_year_percentage_change', 
-        'P/E Ratio TTM' : 'pe_ttm',
-        'Price to Sales TTM' : 'ps_ttm', 
-        'Price to Cash Flow MRQ' : 'pcf', 
-        'Price to Free Cash Flow TTM' : 'pcf_ttm', 
-        'Price to Book MRQ' : 'pb', 
-        '5 Year EPS Growth 5YA' : 'five_year_eps_growth',
-        '5 Year Sales Growth 5YA' : 'five_year_sales_growth', 
-        '5 Year Capital Spending Growth 5YA' : 'five_year_capital_spending_growth',
-        'Asset Turnover TTM' : 'asset_turnover', 
-        'Inventory Turnover TTM' : 'inventory turnover (ttm)', 
-        'Receivable Turnover TTM' : 'receivable_turnover',
-        'Gross margin TTM' : 'gross_margin', 
-        'Operating margin TTM' : 'operating_margin', 
-        'Net Profit margin TTM' : 'net_profit_margin', 
-        'Quick Ratio MRQ' : 'quick_ratio',
-        'Current Ratio MRQ' : 'current_ratio', 
-        'Total Debt to Equity MRQ' : 'debt_to_equity', 
-        'Dividend Yield 5 Year Avg. 5YA' : 'five_year_dividend_average',
-        'Dividend Growth Rate ANN' : 'dividend_growth_rate', 
-        'Payout Ratio TTM' : 'payout_ratio'
-    }
-    cleaned_data = data[rename_cols.keys()].rename(rename_cols, axis = 1)
+def rename_and_convert(data, period):
+    if period == "monthly":
+        rename_cols = {
+            'Name' : 'name', 
+            'Symbol' : 'symbol',
+            'currency' : 'currency',
+            'Sector' : 'sector', 
+            'Industry' : 'industry', 
+            'Last' : 'close', 
+            'ChgPct' : 'percentage_change',
+            'FundamentalMarketCap' : 'market_cap', 
+            'Volume_x' : 'volume', 
+            'FundamentalRatio' : 'pe', 
+            'FundamentalRevenue' : 'revenue',
+            'EPS' : 'eps',
+            'FundamentalBeta' : 'beta', 
+            'dividend' : 'dividend',
+            'dividend_yield' : 'dividend_yield',
+            'TechnicalDay' : 'daily_signal',
+            'TechnicalWeek' : 'weekly_signal', 
+            'TechnicalMonth' : 'monthly_signal',
+            'PerformanceDay' : 'daily_percentage_change', 
+            'PerformanceWeek' : 'weekly_percentage_change',
+            'PerformanceMonth' : 'monthly_percentage_change', 
+            'PerformanceYtd' : 'ytd_percentage_change',
+            'PerformanceYear' : 'one_year_percentage_change', 
+            'Performance3Year' : 'three_year_percentage_change', 
+            'P/E Ratio TTM' : 'pe_ttm',
+            'Price to Sales TTM' : 'ps_ttm', 
+            'Price to Cash Flow MRQ' : 'pcf', 
+            'Price to Free Cash Flow TTM' : 'pcf_ttm', 
+            'Price to Book MRQ' : 'pb', 
+            '5 Year EPS Growth 5YA' : 'five_year_eps_growth',
+            '5 Year Sales Growth 5YA' : 'five_year_sales_growth', 
+            '5 Year Capital Spending Growth 5YA' : 'five_year_capital_spending_growth',
+            'Asset Turnover TTM' : 'asset_turnover', 
+            'Inventory Turnover TTM' : 'inventory turnover (ttm)', 
+            'Receivable Turnover TTM' : 'receivable_turnover',
+            'Gross margin TTM' : 'gross_margin', 
+            'Operating margin TTM' : 'operating_margin', 
+            'Net Profit margin TTM' : 'net_profit_margin', 
+            'Quick Ratio MRQ' : 'quick_ratio',
+            'Current Ratio MRQ' : 'current_ratio', 
+            'Total Debt to Equity MRQ' : 'debt_to_equity', 
+            'Dividend Yield 5 Year Avg. 5YA' : 'five_year_dividend_average',
+            'Dividend Growth Rate ANN' : 'dividend_growth_rate', 
+            'Payout Ratio TTM' : 'payout_ratio'
+        }
+        cleaned_data = data[rename_cols.keys()].rename(rename_cols, axis = 1)
 
-    cleaned_data.replace(['-', 'N/A'], np.nan, inplace=True)
-    cleaned_data['revenue'] = cleaned_data['revenue'].apply(convert_to_number)
-    cleaned_data['market_cap'] = cleaned_data['market_cap'].apply(convert_to_number)
-    return cleaned_data
+        cleaned_data.replace(['-', 'N/A'], np.nan, inplace=True)
+        cleaned_data['revenue'] = cleaned_data['revenue'].apply(convert_to_number)
+        cleaned_data['market_cap'] = cleaned_data['market_cap'].apply(convert_to_number)
+        return cleaned_data
+    elif period == "daily":
+        rename_cols = {
+            'Symbol' : 'symbol',
+            'Last' : 'close', 
+            'ChgPct' : 'percentage_change',
+            'FundamentalMarketCap' : 'market_cap', 
+            'Volume' : 'volume', 
+            'FundamentalRatio' : 'pe', 
+            'FundamentalRevenue' : 'revenue',
+            'FundamentalBeta' : 'beta', 
+            'TechnicalDay' : 'daily_signal',
+            'TechnicalWeek' : 'weekly_signal', 
+            'TechnicalMonth' : 'monthly_signal',
+            'PerformanceDay' : 'daily_percentage_change', 
+            'PerformanceWeek' : 'weekly_percentage_change',
+            'PerformanceMonth' : 'monthly_percentage_change', 
+            'PerformanceYtd' : 'ytd_percentage_change',
+            'PerformanceYear' : 'one_year_percentage_change', 
+            'Performance3Year' : 'three_year_percentage_change',
+        }
+        cleaned_data = data[rename_cols.keys()].rename(rename_cols, axis = 1)
 
-logging.basicConfig(filename="logs.log", level=logging.INFO)
-data = GetGeneralData("sg")
-links = data["Url"].tolist()
-extension, failed_links = GetAdditionalData(links)
-data_full = pd.merge(data, extension, on = "Url", how = "inner")
-# Retry the failed links
-n_try = 0
-failed_links["links"] = [link.split("?")[0] if "?" in link else link for link in failed_links["links"]]
-while len(failed_links["links"]) != 0 or n_try < 10:
-    print(f"iterasi ke-{n_try+1}")
-    if len(failed_links["links"]) == 0:
-        break
-    new_extension, failed_links = GetAdditionalData(failed_links["links"])
-    n_try += 1
-remaining = data[data["Url"].isin(failed_links["links"])]
-remaining = remaining.assign(Url = [link.split("?")[0] if "?" in link else link for link in failed_links["links"]])
-updated_extension = pd.merge(remaining, new_extension, on = "Url", how = "inner")
-data_final = pd.concat([data_full[~data_full["Url"].isin(failed_links["links"])], updated_extension])
-data_final = rename_and_convert(data_final)
+        cleaned_data.replace(['-', 'N/A'], np.nan, inplace=True)
+        cleaned_data['revenue'] = cleaned_data['revenue'].apply(convert_to_number)
+        cleaned_data['market_cap'] = cleaned_data['market_cap'].apply(convert_to_number)
+        return cleaned_data
+
+def clean_daily_foreign_data(foreign_daily_data):
+    """
+    SGX/KLSE Daily Data Fetching Cleansing.
+
+    Parameters:
+    - foreign_daily_data: dataframe, KLSE/SGX daily data from investing.com api hit
+
+    Returns:
+    - foreign_daily_data: dataframe, Cleaned KLSE/SGX daily data
+    """
+
+    # Replace '-' data with ''
+    foreign_daily_data = foreign_daily_data.replace('-',np.nan)
+
+    # Remove percentage and change data to decimal
+    for i in ["daily",'weekly','monthly','ytd','one_year','three_year']:
+        foreign_daily_data[f"{i}_percentage_change"] = foreign_daily_data[f"{i}_percentage_change"]/100 
+
+    # Rename columns
+    foreign_daily_data.rename(columns={"daily_percentage_change":"change_1d", "weekly_percentage_change":'change_7d', 
+                        "monthly_percentage_change":"change_1m", 
+                        "ytd_percentage_change":"change_ytd",
+                        "one_year_percentage_change":"change_1y",
+                        "three_year_percentage_change":"change_3y"}, inplace=True)
+
+    # Delete redundant percentage change columns
+    foreign_daily_data.drop("percentage_change",axis=1, inplace = True)
+
+    # Change data type to float
+    float_columns = ['close', 'market_cap', 'volume','pe', 'revenue', 'beta','change_1d',
+       'change_7d', 'change_1m', 'change_ytd', 'change_1y', 'change_3y',]
+
+    foreign_daily_data[float_columns] = foreign_daily_data[float_columns].applymap(lambda x:float(str(x).replace(',', '')))
+
+    return foreign_daily_data
+
+def clean_periodic_foreign_data(foreign_periodic_data, foreign_sectors):
+    """
+    SGX/KLSE Periodic Data Fetching Cleansing.
+
+    Parameters:
+    - foreign_periodic_data: dataframe, periodic data from investing.com data scraping using request
+    - foreign_sectors: dataframe, KLSE/SGX sectors mapping to IDX sectors
+
+    Returns:
+    - foreign_periodic_data: dataframe, Cleaned KLSE/SGX periodic data
+    """
+
+    # Replace '-' data with ''
+    foreign_periodic_data = foreign_periodic_data.replace('-',np.nan)
+
+    foreign_periodic_data['dividend_yield'] = foreign_periodic_data['dividend_yield'].apply(lambda x: float(x.strip('%')) / 100 if pd.notnull(x) else np.nan)
+
+    for i in ["gross_margin","operating_margin",'net_profit_margin',"debt_to_equity","five_year_dividend_average",'dividend_growth_rate',"payout_ratio","five_year_eps_growth","five_year_sales_growth","five_year_capital_spending_growth"]:
+        foreign_periodic_data[i] = foreign_periodic_data[i].apply(lambda x: float(x.replace('%', '').replace(',', '')) / 100 if pd.notnull(x) else np.nan)
+
+    foreign_periodic_data.rename(columns={"five_year_dividend_average":"dividend_yield_5y_avg"}, inplace=True) 
+    
+    float_columns = ['eps', 'dividend', 'dividend_yield', 'pe_ttm', 'ps_ttm', 'pcf', 'pcf_ttm', 'pb', 'five_year_eps_growth',
+       'five_year_sales_growth', 'five_year_capital_spending_growth',
+       'asset_turnover', 'inventory turnover (ttm)', 'receivable_turnover',
+       'gross_margin', 'operating_margin', 'net_profit_margin', 'quick_ratio',
+       'current_ratio', 'debt_to_equity', 'dividend_yield_5y_avg',
+       'dividend_growth_rate', 'payout_ratio']
+
+    foreign_periodic_data[float_columns] = foreign_periodic_data[float_columns].applymap(lambda x:float(str(x).replace(',', '')))
+
+    foreign_periodic_data = foreign_periodic_data.merge(foreign_sectors, on = ["sector",'industry']).drop(["sector",'industry'], axis=1).rename(columns={"sectors_id":"sector","sub_sector_id":"sub_sector"})
+
+    return foreign_periodic_data
+
+# logging.basicConfig(filename="logs.log", level=logging.INFO)
+# data = GetGeneralData("sg")
+# links = data["Url"].tolist()
+# extension, failed_links = GetAdditionalData(links)
+# data_full = pd.merge(data, extension, on = "Url", how = "inner")
+# # Retry the failed links
+# n_try = 0
+# failed_links["links"] = [link.split("?")[0] if "?" in link else link for link in failed_links["links"]]
+# while len(failed_links["links"]) != 0 or n_try < 10:
+#     print(f"iterasi ke-{n_try+1}")
+#     if len(failed_links["links"]) == 0:
+#         break
+#     new_extension, failed_links = GetAdditionalData(failed_links["links"])
+#     n_try += 1
+# remaining = data[data["Url"].isin(failed_links["links"])]
+# remaining = remaining.assign(Url = [link.split("?")[0] if "?" in link else link for link in failed_links["links"]])
+# updated_extension = pd.merge(remaining, new_extension, on = "Url", how = "inner")
+# data_final = pd.concat([data_full[~data_full["Url"].isin(failed_links["links"])], updated_extension])
+# data_final = rename_and_convert(data_final)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Update sg or my data. If no argument is specified, the sg data will be updated.")
     parser.add_argument("-sg", "--singapore", action="store_true", default=False, help="Update singapore data")
     parser.add_argument("-my", "--malaysia", action="store_true", default=False, help="Update malaysia data")
+    parser.add_argument("-d", "--daily", action="store_true", default=False, help="Update daily data")
+    parser.add_argument("-m", "--monthly", action="store_true", default=False, help="Update monthly data")
 
     args = parser.parse_args()
     if args.singapore and args.malaysia:
-        print("Error: Please specify either -a or -q, not both.")
+        print("Error: Please specify either -sg or -my, not both.")
+        raise SystemExit(1)
+    if args.daily and args.monthly:
+        print("Error: Please specify either -d or -m, not both.")
         raise SystemExit(1)
     
+    url_supabase = os.getenv("SUPABASE_URL")
+    key = os.getenv("SUPABASE_KEY")
+    supabase = create_client(url_supabase, key)
     logging.basicConfig(filename="logs.log", level=logging.INFO)
-    country = "my" if args.malaysia else "sg" 
-    data = GetGeneralData(country)
-    links = data["Url"].tolist()
-    extension, failed_links = GetAdditionalData(links)
-    data_full = pd.merge(data, extension, on = "Url", how = "inner")
-    # Retry the failed links
-    n_try = 0
-    failed_links["links"] = [link.split("?")[0] if "?" in link else link for link in failed_links["links"]]
-    while len(failed_links["links"]) != 0 or n_try < 10:
-        print(f"iterasi ke-{n_try+1}")
-        if len(failed_links["links"]) == 0:
-            break
-        new_extension, failed_links = GetAdditionalData(failed_links["links"])
-        n_try += 1
-    remaining = data[data["Url"].isin(failed_links["links"])]
-    remaining = remaining.assign(Url = [link.split("?")[0] if "?" in link else link for link in failed_links["links"]])
-    updated_extension = pd.merge(remaining, new_extension, on = "Url", how = "inner")
-    data_final = pd.concat([data_full[~data_full["Url"].isin(failed_links["links"])], updated_extension])
-    data_final = rename_and_convert(data_final)
-    data.to_csv("data_my.csv", index = False) if args.malaysia else data.to_csv("data_sg.csv", index = False)
+    country = "my" if args.malaysia else "sg"
+    sg_sectors = pd.read_csv("sectors_mapping/sectors_sg.csv", sep = ";")
+    my_sectors = pd.read_csv("sectors_mapping/sectors_my.csv", sep = ";")
+    foreign_sectors = my_sectors if args.malaysia else sg_sectors
+
+    if args.monthly:
+        data = GetGeneralData(country)
+        links = data["Url"].tolist()
+        extension, failed_links = GetAdditionalData(links)
+        data_full = pd.merge(data, extension, on = "Url", how = "inner")
+        # Retry the failed links
+        n_try = 0
+        failed_links["links"] = [link.split("?")[0] if "?" in link else link for link in failed_links["links"]]
+        while len(failed_links["links"]) != 0 or n_try < 10:
+            print(f"iterasi ke-{n_try+1}")
+            if len(failed_links["links"]) == 0:
+                break
+            new_extension, failed_links = GetAdditionalData(failed_links["links"])
+            n_try += 1
+        remaining = data[data["Url"].isin(failed_links["links"])]
+        remaining = remaining.assign(Url = [link.split("?")[0] if "?" in link else link for link in failed_links["links"]])
+        updated_extension = pd.merge(remaining, new_extension, on = "Url", how = "inner")
+        data_final = pd.concat([data_full[~data_full["Url"].isin(failed_links["links"])], updated_extension])
+        data_final = rename_and_convert(data_final, "monthly")
+        data_final = clean_daily_foreign_data(data_final)
+        data_final = clean_periodic_foreign_data(data_final, foreign_sectors)
+    elif args.daily:
+        data = GetGeneralData(country)
+        data = rename_and_convert(data, "daily")
+        data = clean_daily_foreign_data(data)
+        db = "sgx_companies" if args.malaysia else "klse_companies"
+        data_db = supabase.table(db).select("*").execute()
+        data_db = pd.DataFrame(data_db.data)
+        drop_cols = ['close', 'market_cap', 'volume', 'pe',
+        'revenue', 'beta', 'daily_signal', 'weekly_signal',
+        'monthly_signal', 'change_1d', 'change_7d', 'change_1m',
+        'change_ytd', 'change_1y', 'change_3y']
+        data_db.drop(drop_cols, axis = 1, inplace = True)
+        data_final = pd.merge(data, data_db, on = "symbol", how = "inner")
+    data_final.to_csv("data_my.csv", index = False) if args.malaysia else data.to_csv("data_sg.csv", index = False)
