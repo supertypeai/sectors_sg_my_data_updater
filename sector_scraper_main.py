8514eb838e5dace3229638547aa866b868f1c006
Gulilil <juan.csantoso@gmail.com>
2024-08-21 15:05:28 +0700

    chores: refactor code

--- sector_scraper_main.py
+++ sector_scraper_main.py
@@ -1,9 +1,10 @@
 from sector_scraper_klse import scrap_function_my, scrap_null_data_my
 from sector_scraper_sgx import scrap_function_sg, scrap_null_data_sg
-from sector_combiner import combine_data
-import pandas as pd
-from multiprocessing import Process
+import json
 import os
+import pandas as pd
+import numpy as np
+from multiprocessing import Process
 import time
 from dotenv import load_dotenv
 from supabase import create_client
@@ -25,6 +26,50 @@ PARAMS_DICT = {
     "null_handling_function" : scrap_null_data_my
   }
 }
+
+def combine_data(df_db_data : pd.DataFrame, type : str, symbol_column: str):
+  cwd = os.getcwd()
+  data_dir = os.path.join(cwd, "data")
+  data_file_path = [os.path.join(data_dir,f'P{i}_data_{type}.json') for i in range(1,5)]
+
+  # Combine data
+  all_data_list = list()
+  for file_path in data_file_path:
+    f = open(file_path)
+    data = json.load(f)
+    all_data_list = all_data_list + data
+
+  # Make Dataframe
+  df_scraped = pd.DataFrame(all_data_list)
+  
+  # Sort df_db_data and df_scraped
+  df_db_data = df_db_data.sort_values([symbol_column])
+  df_scraped = df_scraped.sort_values([symbol_column])
+
+  indices_list = df_db_data.index.tolist()
+
+  
+  # Save to JSON and CSV
+  cwd = os.getcwd()
+  data_dir = os.path.join(cwd, "data")
+  df_scraped.to_json(os.path.join(data_dir, f"final_data_{type}.json"), orient="records", indent=2)
+  # df_scraped.to_csv(os.path.join(data_dir, f"final_data_{type}.csv"), index=False) # Only activated if needed
+
+  # Reset index
+  df_db_data = df_db_data.reset_index(drop= True)
+  df_scraped = df_scraped.reset_index(drop= True)
+
+  # Merge the dataframe to the one in the db
+  df_db_data.update(df_scraped)
+
+  df_db_data.index = indices_list
+
+  # Replace mp.nan to None
+  df_merge = df_db_data.replace({np.nan: None})
+  
+  return df_merge
+
+
 
 if __name__ == "__main__":
   load_dotenv()
